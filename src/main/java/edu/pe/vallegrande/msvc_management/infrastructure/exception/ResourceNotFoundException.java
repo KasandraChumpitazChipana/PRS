51c9db5e6371e9a556ac31be9028b5a6a5259e53
KasandraRosarioChumpitazChipana <kasandrarosariochumpitazchipan@gmail.com>
2025-09-04 08:29:18 -0500

    listo

--- src/main/java/edu/pe/vallegrande/msvc_management/infrastructure/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package edu.pe.vallegrande.msvc_management.infrastructure.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
